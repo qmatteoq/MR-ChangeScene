7ccb8b113cb5a399f88e3a3bd0861101227296ae
Matteo Pagani <info@qmatteoq.com>
2018-04-24 17:13:27 +0200

    First commit of the project

---
+++ Assets/Scripts/ButtonReceiver.cs
@@ -0,0 +1,17 @@
+﻿using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.Receivers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonReceiver : InteractionReceiver
+{
+    protected override void InputDown(GameObject obj, InputEventData eventData)
+    {
+        if (obj.name == "CircleButton")
+        {
+            SceneManager.LoadScene("Second");
+        }
+    }
+}
